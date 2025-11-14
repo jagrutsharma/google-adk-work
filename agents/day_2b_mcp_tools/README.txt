0a3438d5d83f8aac62f4a015df1e8f973d9460e4
Jagrut <jagrutsharma@gmail.com>
2025-11-13 18:30:08 -0800

    Day 2 - Part b - mcp server

---
+++ agents/day_2b_mcp_tools/README.txt
@@ -0,0 +1,8 @@
+- Activate venv
+source venv/bin/activate
+
+- Install package
+pip install -r day_2b_mcp_tools/requirements.txt
+
+- Verify that google adk is installed
+pip list | grep google-adk
---
+++ scripts/run_cli_day_2b_mcp_tools.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Script to run the agent in interactive CLI mode
+# Note: Type "exit" to exit the interactive mode
+
+# Go to project root
+cd "$(dirname "$0")/.." || exit
+
+# Run CLI
+adk run agents/day_2b_mcp_tools
